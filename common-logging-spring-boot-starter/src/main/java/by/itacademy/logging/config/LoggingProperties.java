14bcf29754187b142c859a54b2f78694d38dcdb9
Andrei Borisov <andrei.borr@gmail.com>
2023-07-05 19:14:38 +0300

    new starter

---
+++ common-logging-spring-boot-starter/src/main/java/by/itacademy/logging/config/LoggingProperties.java
@@ -0,0 +1,20 @@
+package by.itacademy.logging.config;
+
+import jakarta.annotation.PostConstruct;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@Slf4j
+@Data
+@NoArgsConstructor
+@ConfigurationProperties(prefix = "app.service.logging")
+public class LoggingProperties {
+    private boolean enabled;
+
+    @PostConstruct
+    public void init() {
+        log.info("Logging properties initialized {}", this);
+    }
+}
